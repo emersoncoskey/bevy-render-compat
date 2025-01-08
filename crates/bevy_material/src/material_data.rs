0b9fdd90476376da3e167a47d8a06ba6e060b334
Emerson Coskey <emerson@coskey.dev>
2025-01-08 12:22:35 -0800

    WIP

--- crates/bevy_material/src/material_data.rs
+++ crates/bevy_material/src/material_data.rs
@@ -47,7 +47,7 @@ impl<'w, M: Material<P>, P: MaterialPipeline> MaterialData<'w, M, P> {
 
 pub struct PreparedMaterialInstance<'a, M: Material<P>, P: MaterialPipeline> {
     pub main_entity: MainEntity,
-    pub properties: &'a P::Meta,
+    pub properties: &'a P::Properties,
     pub shaders: &'a MaterialShaders<M, P>,
     pub bind_group: &'a MaterialBindGroup<M>,
     pub properties: &'a MaterialProperties<M, P>,
